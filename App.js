2e31a9383ca4715aef08d84891ae5975eeec0eff
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-08-10 09:18:39 +0300

    task arrays

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import React, {useState} from 'react';
 export default function App() {
+  const [task, setTask] = useState(
+    [
+      {"task":"HTML I","done":true, "id": "1"},
+      {"task":"CSS","done":true, "id": "2"},
+      {"task":"Responsive design","done":true, "id": "3"},
+      {"task":"Git","done":true, "id": "4"},
+      {"task":"JavaScript I","done":true, "id": "5"},
+      {"task":"JavaScript II","done":false, "id": "6"}
+      ]
+  )
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.content}></View>
+      <Text>Pomodoro</Text>
       <StatusBar style="auto" />
     </View>
   );
